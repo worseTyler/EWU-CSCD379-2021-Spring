8f215e624157123d337aac6737157f3c443faee1
Tyler Jones <tylerjones321@gmail.com>
2021-04-18 22:42:14 -0700

    Pretty sure I finished everything

--- SecretSanta/src/SecretSanta.Web/ViewModels/GiftViewModel.cs
+++ SecretSanta/src/SecretSanta.Web/ViewModels/GiftViewModel.cs
@@ -1,4 +1,4 @@
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace SecretSanta.Web.ViewModels
 {
@@ -6,8 +6,8 @@ namespace SecretSanta.Web.ViewModels
     {
         public int Id { get; set; }
         public string Title { get; set; } = "";
-        public string Description { get; set; } = "";
-        public string Url { get; set; } = "";
+        public string? Description { get; set; } = "";
+        public string? Url { get; set; } = "";
         public int Priority { get; set; }
         [Display(Name="User")]
         public int UserId { get; set; }
