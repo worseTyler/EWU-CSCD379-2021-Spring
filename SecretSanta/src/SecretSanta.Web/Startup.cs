ba73af9834e2a5bd67b55ae518c65a19b42fa1af
Tyler Jones <tylerjones321@gmail.com>
2021-05-09 17:12:12 -0700

    Changed URL that web project uses for API

--- SecretSanta/src/SecretSanta.Web/Startup.cs
+++ SecretSanta/src/SecretSanta.Web/Startup.cs
@@ -12,7 +12,7 @@ namespace SecretSanta.Web
     {
         private static HttpClient UsersHttpClient { get; } = new()
         {
-            BaseAddress = new Uri("https://localhost:5101/")
+            BaseAddress = new Uri("https://ewu-secretsanta-api.azurewebsites.net")
         };
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
